dd769b471d7259ae6f12fa495b6d7fdbaa2dc7cd
saragadamraviteja <saragadamraviteja@msitprogram.net>
2019-09-25 17:05:49 +0530

    Assignment 3

--- day 3/Assignment 3/Squares.java
+++ day 3/Assignment 3/Squares.java
@@ -1,29 +1,35 @@
-/**
+/**.
  * Given an array of Integers, return an array of the square
- * of each values in the array. 
- * 
+ * of each values in the array.
+ *
  * Input : {1, 2, 3}
  * Output : {1, 4, 9}
- * 
+ *
  * @author Siva Sankar
  * @author Mayank
  */
 
-public class Squares {
+public final class Squares {
+    private Squares() {
+    }
 
-    /**
+    /**.
      * This method returns the array of ints that are squares of each element
      * in the input array.
-     * 
-     * @param nums, the input array.
-     * 
+     *
+     * @param nums the input array.
+     *
      * @return the array of ints that are squares of each element
      * in the input array.
-     * 
+     *
      */
-    public static int[] squared(int[] nums) {
+    public static int[] squared(final int[] nums) {
+        int[] arr = new int[nums.length];
         //  Your code goes here.
-        
-        return null;
+        for (int i = 0; i < nums.length; i++) {
+            arr[i] = nums[i] * nums[i];
+        }
+        return arr;
     }
 }
+
