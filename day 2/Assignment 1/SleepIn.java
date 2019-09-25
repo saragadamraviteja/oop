e924470362666d00a5ad43b9d7eff1c129e6d446
saragadamraviteja <saragadamraviteja@msitprogram.net>
2019-09-25 14:04:19 +0530

    Assignment 1

--- day 2/Assignment 1/SleepIn.java
+++ day 2/Assignment 1/SleepIn.java
@@ -1,40 +1,51 @@
 /**
- * The parameter weekday is true if it is a weekday, and the parameter vacation is true
- * if we are on vacation. We sleep in if it is not a weekday or we're on vacation.
+ * The parameter weekday is true if it is a weekday, and the
+ * parameter vacation is true
+ * if we are on vacation. We sleep in if it is not a weekday
+ * or we're on vacation.
  * Return true if we sleep in.
- * 
+ *
  * Input : sleepIn(false, false)
  * Output : true
- * 
+ *
  * Input : sleepIn(true, false)
  * Output : Output : false
- * 
+ *
  * Input : sleepIn(false, true)
  * Output : true
- * 
+ *
  * @author Siva Sankar
  */
 
-public class SleepIn {
+public final class SleepIn {
+        private SleepIn() {
+    }
 
     /**
-     * The parameter weekday is true if it is a weekday, and the parameter vacation is true
-     * if we are on vacation. We sleep in if it is not a weekday or we're on vacation.
+     * The parameter weekday is true if it is a weekday, and the
+     * parameter vacation is true
+     * if we are on vacation. We sleep in if it is not a weekday or we're
+     * on vacation.
      * Return true if we sleep in.
-     * 
-     * @param weekday, indicates a weekDay of type boolean
-     * @param vacation, indicates true if we are in vacation.
-     * 
+     *
+     * @param weekDay indicates a weekDay of type boolean
+     * @param vacation indicates true if we are in vacation.
+     *
      * @return true if it is not a weekday and we're on vacation.
      */
-    public static boolean sleepIn(boolean weekDay, boolean vacation) {
+
+    public static boolean sleepIn(final boolean weekDay,
+    final boolean vacation) {
         //  Your code goes here.
-        if (weekDay == true && vacation == false) {
-            return false;
-        } 
-        else if (weekDay == false && vacation == true){
+        /*
+        if (!weekDay || vacation) {
             return true;
+        } else {
+            return false;
         }
-        else return true;
+        */
+        return (!weekDay || vacation);
     }
 }
+
+
---
+++ day 3/Assignment 6/Stats.java
@@ -0,0 +1,96 @@
+/**
+ * Given an array of integers Find the mean, median, mode, standard deviation
+ * and variance.
+ * 
+ * Mean
+ * Input : [1,2,3,4,5]
+ * Output : 3.0
+ * 
+ * Median
+ * Input : [1,2,3,4,5]
+ * Output : 3.0
+ * 
+ * Mode
+ * Input : [1,1,2,2,3]
+ * Output : [1,2]
+ * 
+ * Variance
+ * Input : [1,2,3,4,5]
+ * Output : 2.0
+ * 
+ * Standard Deviation
+ * Input : [1,2,3,4,5]
+ * Output : 1.41421356
+ * 
+ * @author Siva Sankar
+ */
+
+import java.util.*;
+
+public class Stats {
+
+    /**
+     * This method should return the mean of the elements of the array.
+     * If the arr is empty (length is 0), return the mean as 0.
+     * @param arr, the input array contians elements.
+     * @return the mean of the array.
+     */
+    public static double mean(int[] arr) {
+        //  Your code goes here....
+
+        return 0.0;
+    }
+
+    /**
+     * This method returns the median of the elements of the array.
+     * Prerequisite : The array should be sorted to find the median.
+     * 
+     * Hint : Arrays.sort(arr) sorts the elements of the array.
+     * 
+     * @param arr, the input array contains random elements (not in sorted order)
+     * @return the median of the elements of the array.
+     */
+    public static double median(int[] arr) {
+        //  Your code goes here...
+
+        return 0.0;
+    }
+
+    /**
+     * This method returns the mode of the array. If there is no mode, then it returns
+     * 0 as the mode.
+     * 
+     * @param arr, the input array.
+     * @return the mode of the array, 0 otherwise if there is no mode.
+     */
+    public static int mode(int[] arr) {
+        //  Your code goes here....
+
+        return 0;
+    }
+
+    /**
+     * This method returns the variance of the input array.
+     * Make use of the mean functon which has already written.
+     * 
+     * @param arr, the input array
+     * @return the variance of the array.
+     */
+    public static double variance(int[] arr) {
+        //  Your code goes here....
+
+        return 0.0;
+    }
+
+    /**
+     * This method returns the standard deviation of the array.
+     * Hint : You can find the square root using Math.sqrt() method.
+     * @param arr, the input array
+     * @return the standard deviation of the input array.
+     */
+    public static double standardDeviation(int[] arr) {
+        //  Your code goes here....
+
+        return 0.0;
+    }
+}
