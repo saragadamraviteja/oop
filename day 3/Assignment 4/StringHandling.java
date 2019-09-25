8adfd9df7726e3918c91695aaef306e976012241
saragadamraviteja <saragadamraviteja@msitprogram.net>
2019-09-25 17:20:25 +0530

    Assignment 4

--- day 3/Assignment 4/StringHandling.java
+++ day 3/Assignment 4/StringHandling.java
@@ -1,23 +1,29 @@
-/**
- * Given an array of Strings, convert each string of the array into 
- * lower case String and then return the array. 
- * 
+/**.
+ * Given an array of Strings, convert each string of the array into
+ * lower case String and then return the array.
+ *
  * Input : {"Hello", "Hi"}
  * Output : {"hello", "hi"}
- * 
+ *
  * @author Siva Sankar
  */
 
-public class StringHandling {
+public final class StringHandling {
+    private StringHnadling() {
+    }
 
-    /**
+    /**.
      * This method returns the array of lower case strings.
-     * @param arr, the input array.
+     * @param arr the input array.
      * @return the array of lower case Strings based on the input parameter.
      */
-    public static String[] lower(String[] arr) {
+    public static String[] lower(final String[] arr) {
         //  your code goes here....
-        
-        return null;
+        String[] arra = new String[arr.length];
+        for (int i = 0; i < arr.length; i++) {
+            arra[i] = arr[i].toLowerCase();
+            }
+        return arra;
     }
 }
+
