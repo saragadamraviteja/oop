472e0ee36f2afa25979487ea1df2cc46c3d858aa
saragadamraviteja <saragadamraviteja@msitprogram.net>
2019-09-30 12:24:48 +0530

    assignment 1

---
+++ day 4 assignment 2/Card.java
@@ -0,0 +1,37 @@
+public class Card {
+    private String title;
+    private String author;
+    private String subject;
+
+    public Card(String title; String author; String subject) {
+        this.title = title;
+        this.author = author;
+        this.subject = subject;
+    }
+    // setTitle
+    public void setTitle(final String title1) {
+        this.title = title1;
+    }
+    // getTitle
+    public String getTitle() {
+        return title;
+    }
+    // setAuthor
+    public void setAuthor(final String author1) {
+        this.author = author1;
+    }
+    // getAuthor
+    public String getAuthor() {
+        return author;
+    }
+    // setSubject
+    public void setSubject(final String subject1) {
+        this.subject = subject1;
+    }
+    // getSubject
+    public String getSubject() {
+        return subject;
+    }
+
+    }
+}
